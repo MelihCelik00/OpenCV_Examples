9996703417d7f3ed5baac2eb594ae13c66a76961
Melih Safa Çelik <45492251+MelihCelik00@users.noreply.github.com>
2020-04-10 17:27:48 +0300

    Error fixed

--- Image Processing/Improve Yourself/cv02.py
+++ Image Processing/Improve Yourself/cv02.py
@@ -3,20 +3,15 @@ import numpy as np
 
 cap = cv2.VideoCapture(0) 
 
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT,800)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH,1000)
-ctr = 0
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,600)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,8000)
+counter=0
 while True:
-    ret, frame = cap.read()
-    # gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-
-
-    cv2.imshow('frame',frame)
-    #cv2.imshow('gray',gray)
-    ctr += 1
-
-    if ctr == 1000:
-        break
-
+	ret, frame = cap.read()
+	cv2.imshow('frame',frame)
+	counter+=1
+	if (cv2.waitKey(1) & 0xFF == ord('q')) or (counter==100):
+		break
+	#print(counter)
 cap.release()
 cv2.destroyAllWindows()
